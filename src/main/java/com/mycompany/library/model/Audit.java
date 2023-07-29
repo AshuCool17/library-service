40a78edf28d17a12c627e240850e0a332fa2bf45
Ashutosh <ashutosh.mahato@gmail.com>
2023-07-29 18:31:58 +0530

    Added Audit class.

---
+++ src/main/java/com/mycompany/library/model/Audit.java
@@ -0,0 +1,18 @@
+package com.mycompany.library.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Audit {
+
+	private long auditId;
+	
+	private List<Long> defaultersId;
+	
+}
