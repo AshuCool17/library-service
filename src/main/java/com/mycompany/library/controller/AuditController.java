1455e2e187905740feff8788eecae7e764d10aa5
Ashutosh <ashutosh.mahato@gmail.com>
2023-07-30 11:40:27 +0530

    Added AuditController

---
+++ src/main/java/com/mycompany/library/controller/AuditController.java
@@ -0,0 +1,35 @@
+package com.mycompany.library.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.library.model.Audit;
+import com.mycompany.library.model.User;
+
+@RestController(value = "/audit")
+public class AuditController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(AuditController.class);
+
+	@Autowired
+	private AuditService auditService;
+	
+	@GetMapping(value = "/reports")
+	public ResponseEntity<List<User>> getAllUsers(){
+		
+		LOGGER.info("Getting all users info-->");
+		List<Audit> audits = auditService.getReports();
+		if(audits.size() == 0)
+			LOGGER.info("No records found");
+		else
+			LOGGER.info("Successfully retrieved all users info");
+		return new ResponseEntity<>(audits, HttpStatus.OK);
+	}
+}
