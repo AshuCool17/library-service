f3ccfc8cadfa5d8227f23a873672da45d7cce9f4
Ashutosh <ashutosh.mahato@gmail.com>
2023-07-31 12:33:14 +0530

    Added AuditService

---
+++ src/main/java/com/mycompany/library/service/AuditService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.mycompany.library.service;
+
+import java.util.List;
+
+import com.mycompany.library.model.Audit;
+
+/**
+ * @author Ashutosh
+ *
+ */
+public interface AuditService {
+
+	List<Audit> getReports();
+
+	
+}
