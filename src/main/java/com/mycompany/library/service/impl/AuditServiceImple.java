b454cc558a496cf0776986e339eba4d0b4343684
Ashutosh <ashutosh.mahato@gmail.com>
2023-08-01 23:08:26 +0530

    Added AuditServiceImpl

---
+++ src/main/java/com/mycompany/library/service/impl/AuditServiceImple.java
@@ -0,0 +1,16 @@
+package com.mycompany.library.service.impl;
+
+import java.util.List;
+
+import com.mycompany.library.model.Audit;
+import com.mycompany.library.service.AuditService;
+
+public class AuditServiceImple implements AuditService {
+
+	@Override
+	public List<Audit> getReports() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
