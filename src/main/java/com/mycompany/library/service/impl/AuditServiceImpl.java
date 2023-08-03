cf8b82df94fa78ee9274ec51ea5e6cd09a13fcd4
Ashutosh <ashutosh.mahato@gmail.com>
2023-08-03 09:42:17 +0530

    Added AuditDao import

--- src/main/java/com/mycompany/library/service/impl/AuditServiceImpl.java
+++ src/main/java/com/mycompany/library/service/impl/AuditServiceImpl.java
@@ -2,22 +2,17 @@ package com.mycompany.library.service.impl;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.mycompany.library.dao.LibraryDao;
+import com.mycompany.library.dao.AuditDao;
 import com.mycompany.library.model.Audit;
 import com.mycompany.library.service.AuditService;
 
 public class AuditServiceImpl implements AuditService {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(AuditServiceImpl.class);
-
 	private AuditDao auditDao;
 	
 	@Override
 	public List<Audit> getReports() {
-		return null;
+		return auditDao.findAll();
 	}
 
 }
