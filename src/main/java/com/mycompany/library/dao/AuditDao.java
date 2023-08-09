5b2eeb2db47f1c0a05acdfec2403e449350ad5fe
Ashutosh <ashutosh.mahato@gmail.com>
2023-08-09 11:07:04 +0530

    Added Dao query for audit reports between dates

--- src/main/java/com/mycompany/library/dao/AuditDao.java
+++ src/main/java/com/mycompany/library/dao/AuditDao.java
@@ -3,7 +3,12 @@
  */
 package com.mycompany.library.dao;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.mycompany.library.model.Audit;
@@ -15,4 +20,6 @@ import com.mycompany.library.model.Audit;
 @Repository
 public interface AuditDao extends JpaRepository<Audit, Long> {
 
+	@Query(value = "SELECT * FROM Audit WHERE LAST_UPDATED >= :startDate AND LAST_UPDATED <= :endDate", nativeQuery = true)
+	List<Audit> getAllBetweenDates(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
