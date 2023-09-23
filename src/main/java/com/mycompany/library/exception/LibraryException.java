2f291908bb120890ab10b4c063c676c417fb2ae2
Ashutosh <ashutosh.mahato@gmail.com>
2023-09-23 11:22:55 +0530

    Added Library Exception class

---
+++ src/main/java/com/mycompany/library/exception/LibraryException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.mycompany.library.exception;
+
+/**
+ * @author Ashutosh
+ *
+ */
+public class LibraryException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public LibraryException() {
+		super();
+	}
+}
