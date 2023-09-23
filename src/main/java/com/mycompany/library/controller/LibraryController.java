1ec8e15962e4714e93b0f2cce277fa36763abcae
Ashutosh <ashutosh.mahato@gmail.com>
2023-09-23 11:23:57 +0530

    Added class - Library Exception

--- src/main/java/com/mycompany/library/controller/LibraryController.java
+++ src/main/java/com/mycompany/library/controller/LibraryController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mycompany.library.exception.LibraryException;
 import com.mycompany.library.model.Book;
 import com.mycompany.library.service.LibraryService;
 
