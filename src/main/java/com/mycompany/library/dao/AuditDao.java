e58d54ae33c25bbce0c79a13948e3b19bbe862ba
Ashutosh <ashutosh.mahato@gmail.com>
2023-10-09 23:59:27 +0530

    Import missing

--- src/main/java/com/mycompany/library/dao/AuditDao.java
+++ src/main/java/com/mycompany/library/dao/AuditDao.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.mycompany.library.exception.AuditNotFoundException;
 import com.mycompany.library.model.Audit;
 
 /**
