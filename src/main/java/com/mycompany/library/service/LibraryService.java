f9d907cbbaa8220a63edda858df445957f8cd96d
Ashutosh <ashutosh.mahato@gmail.com>
2023-12-09 22:29:24 +0530

    Added exception

--- src/main/java/com/mycompany/library/service/LibraryService.java
+++ src/main/java/com/mycompany/library/service/LibraryService.java
@@ -32,6 +32,6 @@ public interface LibraryService {
 
 	List<String> upcomingBooks() throws LibraryException;//displays upcoming books
 
-	Integer getNumberOfCopies();
+	Integer getNumberOfCopies() throws LibraryException;//retrieves number of copies available
 
 }
