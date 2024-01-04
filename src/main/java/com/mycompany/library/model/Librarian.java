6c1ed2dca5c5b186763942864c1cc36dfaa5ae77
Ashutosh <ashutosh.mahato@gmail.com>
2024-01-04 19:36:04 +0530

    Added librarian pojo

---
+++ src/main/java/com/mycompany/library/model/Librarian.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.mycompany.library.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Ashutosh
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Librarian {
+
+	private long id;
+	
+	private String name;
+}
