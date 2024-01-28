ed158e1c611a38fd29ffce38a5413ae6b0ecf2ec
Ashutosh <ashutosh.mahato@gmail.com>
2024-01-28 19:36:20 +0530

    Added get user by name DAO

--- src/main/java/com/mycompany/library/dao/UserDao.java
+++ src/main/java/com/mycompany/library/dao/UserDao.java
@@ -3,6 +3,8 @@
  */
 package com.mycompany.library.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -18,4 +20,7 @@ public interface UserDao extends JpaRepository<User, Long>{
 
 	@Query(value = "select user.name from book")
 	public User getUserForBookIssued(String bookName);
+
+	@Query(value = "select user from users where name: name")
+	public Optional<User> getUserByName(String name);
 }
