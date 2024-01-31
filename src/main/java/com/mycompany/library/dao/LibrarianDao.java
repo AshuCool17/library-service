1335ed88d1aeedee889a29bf00bb35d7cb20c7ce
Ashutosh <ashutosh.mahato@gmail.com>
2024-01-31 23:38:12 +0530

    Added getLibrarianByName DAO

--- src/main/java/com/mycompany/library/dao/LibrarianDao.java
+++ src/main/java/com/mycompany/library/dao/LibrarianDao.java
@@ -1,9 +1,15 @@
 package com.mycompany.library.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mycompany.library.model.Librarian;
+import com.mycompany.library.model.User;
 
 public interface LibrarianDao extends JpaRepository<Librarian, Long>{
 
+	@Query(value = "select * from librarian where name: name")
+	public Optional<Librarian> getLibrarianByName(String name);
 }
